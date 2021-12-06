dc17409eff0094b4207caf149023e74cc3067a03
vic1267 <95277480+vic1267@users.noreply.github.com>
2021-12-06 14:33:30 +0300

    Add files via upload

---
+++ urok.py
@@ -0,0 +1,11 @@
+import pyttsx3
+import time
+
+go = pyttsx3.init()
+go.say('Привет Анна')
+go.say('Как у тебя дела?')
+go.say('что купила в магазине?')
+go.say('ты узнала меня?')
+go.runAndWait()
+
+
